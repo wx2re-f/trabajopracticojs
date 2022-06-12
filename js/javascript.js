cfbd3c6552a67cbae07bf52f33a584f8bcca018e
wx2re-f <104399033+wx2re-f@users.noreply.github.com>
2022-06-12 02:17:25 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+
+
+
+/*modo oscuro*/
+function cambiarfondodocumento() {
+  document.body.style.backgroundColor="black";
+
+
+
+      }
+
+
+
+
+
+    /*footer*/
+
+    /*cartel de informacion*/
+    function cartel() {alert("Autora:Paola Romero - Email:paolaromerocreatividadind@gmail.com")
+
+    }
